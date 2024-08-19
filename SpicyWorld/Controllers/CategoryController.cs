8bde47d2cbf673d090760810def32afef3f3cbae
kaushalya weerakoon <harshanathwak@gmail.com>
2024-08-19 09:22:06 +0530

    Add delete option

--- SpicyWorld/Controllers/CategoryController.cs
+++ SpicyWorld/Controllers/CategoryController.cs
@@ -11,7 +11,7 @@ namespace SpicyWorld.Controllers
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
-                
+
         }
         public IActionResult Index()
         {
@@ -20,27 +20,6 @@ namespace SpicyWorld.Controllers
         }
         public IActionResult Create()
         {
-            
-            return View();
-        }
-
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0) { 
-              return NotFound();
-            }
-            Category cat = _db.Categoiries.Find(id);//this work on primary key
-            //can use first or default or where , if primary key is not only retreive condition 
-            if (cat == null)
-            {
-                return NotFound();
-            }
-
-            return View(cat);
-        }
-
-        public IActionResult Delete()
-        {
 
             return View();
         }
@@ -48,7 +27,8 @@ namespace SpicyWorld.Controllers
         [HttpPost]
         public IActionResult Create(Category category)
         {
-            if (category.Name != null && category.Description != null && category.Name.ToLower().Equals(category.Description.ToLower())) {
+            if (category.Name != null && category.Description != null && category.Name.ToLower().Equals(category.Description.ToLower()))
+            {
                 ModelState.AddModelError("name", "Cannot have same name and description");
             }
             if (category.Name != null && category.Name.ToLower().Equals("test"))
@@ -64,6 +44,22 @@ namespace SpicyWorld.Controllers
             }
             return View();
 
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category cat = _db.Categoiries.Find(id);//this work on primary key
+            //can use first or default or where , if primary key is not only retreive condition 
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            return View(cat);
         }
 
         [HttpPost]
@@ -85,6 +81,37 @@ namespace SpicyWorld.Controllers
 
             }
             return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category cat = _db.Categoiries.Find(id);//this work on primary key
+            //can use first or default or where , if primary key is not only retreive condition 
+            if (cat == null)
+            {
+                return NotFound();
+            }
+
+            return View(cat);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteCategory(int? id)
+        {
+            var category = _db.Categoiries.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categoiries.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
 
         }
     }
